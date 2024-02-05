f792a2656aac66cc4d8cc68c6f67ca5b3478316d
Food-Is-Good <Henry.zhangyy.sh@gmail.com>
2024-02-05 11:00:56 -0600

    Updated saveQuestions
    Added recursive preOrder

--- src/QuestionsGame.java
+++ src/QuestionsGame.java
@@ -1,12 +1,65 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
 // This is a starter file for QuestionsGame.
 //
 // You should delete this comment and replace it with your class
 // header comment.
 
 public class QuestionsGame {
-    // Your code here
+	
+	QuestionNode overallRoot;
+	
+	public QuestionsGame(String object) {
+		overallRoot = new QuestionNode(object); 
+	}
+	
+	public QuestionsGame(Scanner scanner) {
+		overallRoot = new QuestionNode(scanner.nextLine());
+	}
+	
+	public void saveQuestions(PrintStream output) {
+		QuestionNode root = overallRoot;
+		while(root!=null) {
+			output.println(preOrder(overallRoot));
+		}
+	}
+	public String preOrder(QuestionNode root) {
+		
+		if(root!=null&& root.left==null&& root.right==null) {//leaves
+			return root.data;
+		}
+		if(root!=null&& root.left==null&& root.right!=null) {//root with right only
+			return root.data+"\n"+preOrder(root.right);
+		}
+		if(root!=null&& root.left!=null&& root.right==null) {//root with left only
+			return root.data+"\n"+preOrder(root.left);
+		}
+		if(root!=null && root.left!=null && root.right!=null) {//root with left and right
+			return(root.data+"\n"+preOrder(root.left)+"\n"+preOrder(root.right));
+		}
+		else
+			return "";
+	}
+    
+	public void play() {
+		
+	}
 
-    private static class QuestionNode {
-        // Your code here
+    public static class QuestionNode {
+    	
+        public final String data;
+        public QuestionNode left;
+        public QuestionNode right;
+        
+        public QuestionNode(String data) {
+        	this(data, null, null);
+        }
+        
+        public QuestionNode(String data, QuestionNode left, QuestionNode right) {
+        	this.data = data;
+        	this.left = left;
+        	this.right = right;
+        }
     }
 }
